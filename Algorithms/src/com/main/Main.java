3c2405085ed9e875f928c8adbe756ab9d9958ecf
Tarun Sunkaraneni <tarun.sunkaraneni@gmail.com>
2018-06-25 23:21:54 -0600

    Implemented weighted QuickUnionUF

--- Algorithms/src/com/main/Main.java
+++ Algorithms/src/com/main/Main.java
@@ -3,13 +3,14 @@ package com.main;
 import week1.QuickFindUF;
 import week1.QuickUnionUF;
 import week1.UF;
+import week1.WeightedQuickUnionUF;
 
 public class Main {
 
     public static void main(String[] args) {
 	int N = 10;
 	int[][] unions = new int[][] {{4,3},{3,8},{6,5},{9,4},{2,1},{8,9},{5,0},{7,2},{6,1},{1,0},{6,7}};
-	UF uf = new QuickFindUF(N);
+	UF uf = new WeightedQuickUnionUF(N);
 	for (int[] pair : unions) {
 	    int p = pair[0];
 	    int q = pair[1];
--- Algorithms/src/week1/WeightedQuickUnionUF.java
+++ Algorithms/src/week1/WeightedQuickUnionUF.java
@@ -1,22 +1,32 @@
 package week1;
 
-
-public class QuickUnionUF implements UF {
+public class WeightedQuickUnionUF implements UF {
     int[] id;
+    int[] weight;
     int count;
-    public QuickUnionUF(int n) {
+    public WeightedQuickUnionUF(int n) {
         id = new int[n];
+        weight = new int[n];
         count = n;
         for (int i = 0; i < n; i++) {
             id[i] = i;
+            weight[i] = 1;
         }
     }
     @Override
     public void union(int p, int q) {
         int pid = root(p);
         int qid = root(q);
+
+        // this makes sure that the larger component will always be the parent
         if (pid != qid) {
-            id[qid] = pid;
+            if (weight[pid] > weight[qid]) {
+                id[qid] = pid;
+                weight[pid] += weight[qid];
+            } else {
+                id[pid] = qid;
+                weight[qid] += weight[pid];
+            }
             count--;
         }
     }
